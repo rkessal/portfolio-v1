856b69d4819adc4d03c28ca5e08a5dff05fb6058
rkessal <kessal.rayhan25@gmail.com>
2022-10-01 22:46:08 +0200

    change contact email

--- components/footer.js
+++ components/footer.js
@@ -5,8 +5,8 @@ import Section from "../layouts/section";
 export default function Footer() {
   return (
     <footer className="border-t-[1px] py-4 flex flex-col items-start text-center space-y-5 text-redMain lg:flex-row lg:justify-between lg:items-center lg:space-y-0">
-      <a href="mailto:kessal.rayhan25@gmail.com" aria-label="contact">
-        kessal.rayhan25@gmail.com
+      <a href="mailto:hello@errka.dev" aria-label="contact">
+        hello@errka.dev
       </a>
       <h3>Besancon, FR</h3>
       <div className="flex flex-row space-x-2">
