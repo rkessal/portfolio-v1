05e9d98444eca32b49d84f2759a32855bef0e299
rkessal <kessal.rayhan25@gmail.com>
2022-10-05 22:13:45 +0200

    add manage project

--- pages/manage.js
+++ pages/manage.js
@@ -1,26 +1,27 @@
 import BackToHome from "../components/backToHome";
 import ProjectShowcase from "../components/projectShowcase";
 import Project from "../layouts/project";
-import Section from "../layouts/section";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 import Image from "next/image";
+import Section from "../layouts/section";
 
-export default function Easybank() {
+export default function Manage() {
   const project = {
-    title: "Easybank",
-    desc: "Ce projet est un challenge du site \"frontendmentor\", le but étant de reproduire un site web en s'inspirant des images qui nous sont fournies. C'est un site qui présente les services pour une banque qui s'appelle Easybank. Durant ce projet, j'ai appri à utiliser le framework ChakraUI. ",
-    stack: ["NextJS", "ChakraUI"],
+    id: 5,
+    title: "Manage Website",
+    desc: "Ce projet est un challenge du site \"frontendmentor\", le but étant de reproduire un site web en s'inspirant des images qui nous sont fournies. C'est un site qui présente un outil de gestion qui s'appelle Manage. Durant ce projet, j'ai appri à utiliser le framework TailwindCSS.",
+    stack: ["ReactJS", "TailwindCSS"],
     links: [
       {
-        site: "https://easy-bank-website-rkessal.vercel.app/",
-        repo: "https://github.com/rkessal/easy-bank-website",
+        site: "http://rkessal.github.io/manage-website",
+        repo: "https://www.github.com/rkessal/manage-website",
       },
     ],
   };
 
   return (
-    <Project title="Easybank" desc="Projet Easybank">
+    <Project title="Manage" desc="Projet Manage">
       <BackToHome />
       <Section>
         <ProjectShowcase
@@ -28,11 +29,12 @@ export default function Easybank() {
           desc={project.desc}
           stack={project.stack}
           links={project.links}
+          key={project.id}
         >
           <div className="rounded-lg shadow-lg overflow-hidden w-full md:col-span-4 ">
             <Zoom>
               <Image
-                src="/easy-bank.png"
+                src="/manage.png"
                 layout="responsive"
                 width={1865}
                 height={965}
@@ -42,7 +44,7 @@ export default function Easybank() {
           <div className="rounded-lg shadow-lg overflow-hidden w-full md:col-span-1">
             <Zoom>
               <Image
-                src="/easybank-mobile.png"
+                src="/manage-mobile.png"
                 layout="responsive"
                 width={437}
                 height={965}
@@ -52,7 +54,7 @@ export default function Easybank() {
           <div className="rounded-lg shadow-lg overflow-hidden w-full md:col-span-4 md:order-2">
             <Zoom>
               <Image
-                src="/easybank-2.png"
+                src="/manage-2.png"
                 layout="responsive"
                 width={1865}
                 height={965}
@@ -62,27 +64,7 @@ export default function Easybank() {
           <div className="rounded-lg shadow-lg overflow-hidden w-full md:col-span-1">
             <Zoom>
               <Image
-                src="/easybank-2-mobile.png"
-                layout="responsive"
-                width={437}
-                height={965}
-              />
-            </Zoom>
-          </div>
-          <div className="rounded-lg shadow-lg overflow-hidden w-full md:col-span-4 md:order-4">
-            <Zoom>
-              <Image
-                src="/easybank-3.png"
-                layout="responsive"
-                width={1865}
-                height={965}
-              />
-            </Zoom>
-          </div>
-          <div className="rounded-lg shadow-lg overflow-hidden w-full md:col-span-1 md:order-5">
-            <Zoom>
-              <Image
-                src="/easybank-3-mobile.png"
+                src="/manage-2-mobile.png"
                 layout="responsive"
                 width={437}
                 height={965}
